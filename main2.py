7af7a7c4d4743ded9f9b93d3734767438f772551
victorfpaulino <91426167+victorfpaulino@users.noreply.github.com>
2026-08-07 19:26:45 -0300

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,11 +1,10 @@
-from conta import Conta
+#from conta import Conta
 from conta_corrente import ContaCorrente
 from conta_poupanca import ContaPoupanca
 
 opcao = " "
 contas = []
-contas_cc = []
-contas_cp = []
+
 
 while opcao != "3":
 
@@ -21,40 +20,68 @@ while opcao != "3":
         nome = input("Digite seu nome: ")
         saldo = float(input("Digite o saldo a ser depositado: R$"))
         tipo = input("Digite o tipo de conta: 'CP' para Poupança e 'CC' para Corrente: ")
+        print(" ")
 
-
+        # Se for Conta Poupança
         if tipo == 'CP':
             conta_poupanca = ContaPoupanca(nome, saldo)
-            contas_cp.append(conta_poupanca)
-            contas.append(contas_cp) # Adicionando a CP na lista CONTAS
-            print(f"Parabéns {conta_poupanca.nome}, conta poupança criada com sucesso!")
+            contas.append(conta_poupanca)
+            print(f"Parabéns {conta_poupanca.nome}! Sua conta poupança foi criada com sucesso!")
+            conta_poupanca.extrato_conta_poupanca()
             print(" ")
-            #conta_poupanca.extrato_conta_poupanca()
 
-            # printando todas as contas poupanças
-            i = 0
-            for conta_poupanca in contas_cp:
-                contas_cp[i].extrato_conta_poupanca()
-                #print(contas_cp[i])
-                i+=1
-            print(" ")
-               
+        # Se for Conta Corrente
         elif tipo == "CC":
             conta_corrente = ContaCorrente(nome, saldo)
-            contas_cc.append(conta_corrente)
-            contas.append(contas_cc) # Adicionando a CC na lista de CONTAS
-            print(f"Parabéns {conta_corrente.nome}, conta corrente criada com sucesso!")
-            print(" ")
-            #conta_corrente.extrato_conta_corrente()
-            j = 0
-            for conta_corrente in contas_cc:
-                contas_cc[j].extrato_conta_corrente()
-                j+=1
+            contas.append(conta_corrente)
+            print(f"Parabéns {conta_corrente.nome}! Sua conta corrente foi criada com sucesso!")
+            conta_corrente.extrato_conta_corrente()
             print(" ")       
 
     if opcao == "2":
-        nome = input("Digite seu nome: ")
-        
+        print("*** LOGIN ***")
+        nome_login = input("Digite seu nome: ")
+        print(" ")
+        conta_logada = None
+
+        for obj in contas:
+            if obj.nome == nome_login:
+                conta_logada = obj
+                print("Login efetuado com sucesso!")
+                print(" ")
+                break
+
+        if conta_logada is None:
+            print("ERRO! Usuário não encontrado!")
+        else:
+            print("O que deseja fazer?")
+            print("1. Ver extrato")
+            print("2. Sacar")
+            print("3. Depositar")
+            print("4. Sair")
+
+            op = input("Entre com a opção desejada: ")
+
+            if op == "1":
+                if conta_logada is conta_poupanca:
+                    conta_logada.extrato_conta_poupanca()
+                elif conta_logada is conta_corrente:
+                    conta_logada.extrato_conta_corrente()
+
+            elif op == "2":
+                if conta_logada is conta_poupanca:
+                    valor = float(input("Qual valor deseja sacar? R$"))
+                    conta_logada.sacar_conta_poupanca(valor)
+                elif conta_logada is conta_corrente:
+                    valor = float(input("Qual valor deseja sacar? R$"))
+                    conta_logada.sacar_conta_corrente(valor)
+
+            elif op == "3":
+                   valor = float(input("Qual valor deseja depositar? R$"))
+                   conta_logada.depositar(valor)
+
+            elif op == "4":
+                break
 
 
 print("Acabou!")
